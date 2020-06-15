e6f2bc125231d86b35a588bb58f3ee86ab451b0b
qwert830 <qwertaudrb@naver.com>
2020-06-15 20:57:10 +0900

    2020-06-15 숫자게임

---
+++ 숫자 게임/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[4] { 5, 1, 3, 7 };
+            int[] b = new int[4] { 2, 2, 6, 8 };
+
+            var t = solution(a, b);
+            Console.Write(t);
+        }
+
+        public static int solution(int[] A, int[] B)
+        {
+            int answer = 0;
+
+            Array.Sort(A);
+            Array.Sort(B);
+
+            int length = A.Length;
+
+            for(int i =0; i<length;)
+            {
+                for(int j = 0; j<length;)
+                {
+                    if(A[i]>=B[j])
+                    {
+                        j++;
+                    }
+                    else
+                    {
+                        i++;
+                        j++;
+                        answer++;
+                    }
+                }
+                break;
+            }
+
+            return answer;
+        }
+    }
+}
